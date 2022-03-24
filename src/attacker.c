2d0beea667a819782ffa55309d962c471ae5dc7f
Daniel Pessoa Cardeal <cardeal.daniel@usp.br>
2022-03-24 15:22:10 -0300

    Implement attacker's strategy.

--- src/attacker.c
+++ src/attacker.c
@@ -16,14 +16,38 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
-direction_t execute_attacker_strategy(
-    position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
+direction_t execute_attacker_strategy(position_t attacker_position,
+                                      Spy defender_spy) {
+  // The implemented strategy is very simplistic and works as follows:
+  //
+  //   1) At the attacker's turn, he tries the first direction in the direction
+  //      attempt list.
+  //   2) If the movement fail (i.e. the attacker_position hasn't changed), the
+  //      attacker tries the next direction in the attempt list.
+  //   3) If the movement doesn't fail (i.e. the attacker_position has changed),
+  //      the function returns to step 1.
+  //
+  // Note that, although simple, this strategy should be capable to avoid
+  // obstacles and enable the attacker to cross the board, even if not
+  // efficiently.
   UNUSED(defender_spy);
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  const direction_t dir_attempt_sequence[8] = {
+      DIR_DOWN_RIGHT, DIR_RIGHT, DIR_UP_RIGHT,  DIR_UP,
+      DIR_UP_LEFT,    DIR_LEFT,  DIR_DOWN_LEFT, DIR_DOWN};
+
+  static position_t last_pos = {0, 0};
+  static int last_attempt_idx = 7;
+
+  // Choose new direction to try based on result of the last movement (failed or
+  // success)
+  last_attempt_idx = equal_positions(last_pos, attacker_position)
+                         ? (last_attempt_idx + 1) % 8 // Failed to move
+                         : 0;
+  const direction_t next_dir = dir_attempt_sequence[last_attempt_idx];
+
+  last_pos = attacker_position;
+  return next_dir;
 }
 
 /*----------------------------------------------------------------------------*/
